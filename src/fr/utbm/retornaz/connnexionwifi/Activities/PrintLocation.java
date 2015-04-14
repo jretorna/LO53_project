bd90560b106fe9d73de6337ea5d47fceb5ec7f03
jretorna <jeremy.retornaz@orange.fr>
2015-04-14 13:00:35 +0200

    [JR] block the screenRotation and try to add a marker on the map

--- src/fr/utbm/retornaz/connnexionwifi/Activities/PrintLocation.java
+++ src/fr/utbm/retornaz/connnexionwifi/Activities/PrintLocation.java
@@ -1,9 +1,13 @@
 package fr.utbm.retornaz.connnexionwifi.Activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import fr.utbm.retornaz.connnexionwifi.Positioning.TouchView;
 
 public class PrintLocation extends Activity {
 
@@ -17,12 +21,36 @@ public class PrintLocation extends Activity {
 		setContentView(R.layout.print_location);
 
 		this.mapImageView = (ImageView) findViewById(R.id.imageViewMap);
+		this.settingBtn = (ImageButton) findViewById(R.id.settingViewerBtn);
 		this.startLocation = (ImageButton) findViewById(R.id.locateMeBtn);
 
-	}
+		initListeners();
 
+	}
 	private void initListeners() {
-		// this.mapImageView
-	}
+		/*----- Setting -----*/
 
+		this.settingBtn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(final View v) {
+				startActivity(new Intent(PrintLocation.this, Setting.class));
+			}
+		});
+
+		/* ---- StartLocation ---- */
+
+		this.startLocation.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(final View v) {
+				// Start of the localisation
+				TouchView touchView = new TouchView(getApplicationContext());
+				// getWindow().setFlags(WindowManager.LayoutParams.ALPHA_CHANGED,
+				// WindowManager.LayoutParams.ALPHA_CHANGED);
+				// PrintLocation.this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+				// setContentView(touchView);
+				v.draw(touchView.getCanvas());
+
+			}
+		});
+	}
 }
---
+++ src/fr/utbm/retornaz/connnexionwifi/Positioning/TouchView.java
@@ -0,0 +1,56 @@
+package fr.utbm.retornaz.connnexionwifi.Positioning;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+import android.view.View;
+import fr.utbm.retornaz.connnexionwifi.Activities.R;
+
+public class TouchView extends View {
+	private float imageX;
+	private float imageY;
+	private Canvas canvasReturn;
+
+	// Constructeur
+	public TouchView(final Context context) {
+		super(context);
+	}
+
+	@Override
+	public boolean onTouchEvent(final MotionEvent event) {
+		// Pour récupérer la position du doigt sur l'écrans
+		float positionTouchX = event.getX();
+		float positionTouchY = event.getY();
+
+		imageX = positionTouchX;
+		imageY = positionTouchY;
+		return true;
+
+	}
+
+	protected void onDraw(final Canvas canvas) {
+		Bitmap monImage = BitmapFactory.decodeResource(getResources(),
+				R.drawable.map_marker);
+
+		canvas.drawBitmap(monImage, imageX, imageY, null);
+
+		invalidate(); // Efface pour redessiner.
+		canvasReturn = canvas;
+	}
+
+	// Cette fonction est appeler lorsque la taille de l'écran change, par
+	// exemple si il y a rotation de l'écran.
+	@Override
+	public void onSizeChanged(	final int w,
+								final int h,
+								final int oldW,
+								final int oldH) {
+
+	}
+
+	public Canvas getCanvas() {
+		return canvasReturn;
+	}
+}
